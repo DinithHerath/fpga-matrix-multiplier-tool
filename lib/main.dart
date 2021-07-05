85cea675e603b8726d2f9ea3bacbff426c09ae9b
Dinith Herath <dinithherath18332@gmail.com>
2021-07-06 03:04:33 +0530

    Added settings bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fpga_matrix_multiplier/home/bloc/home_provider.dart';
 import 'package:fpga_matrix_multiplier/theme.dart';
+
+import 'settings/bloc/settings_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,11 +13,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FPGA Matrix Multiplier',
-      theme: buildThemeData(context),
-      debugShowCheckedModeBanner: false,
-      home: HomeProvider(),
+    return BlocProvider(
+      create: (context) => SettingsBloc(context),
+      child: MaterialApp(
+        title: 'FPGA Matrix Multiplier',
+        theme: buildThemeData(context),
+        debugShowCheckedModeBanner: false,
+        home: HomeProvider(),
+      ),
     );
   }
 }
