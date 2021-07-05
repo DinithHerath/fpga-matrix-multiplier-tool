059a161ece0cfcc701fbe7e606efe8eca25a715b
Dinith Herath <dinithherath18332@gmail.com>
2021-07-06 03:05:10 +0530

    Randomize matrix added

--- lib/home/bloc/home_bloc.dart
+++ lib/home/bloc/home_bloc.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fpga_matrix_multiplier/settings/bloc/settings_bloc.dart';
 import 'package:process_run/shell.dart';
 
 import 'home_event.dart';
@@ -10,7 +13,11 @@ import 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   Timer timer;
-  HomeBloc(BuildContext context) : super(HomeState.initialState);
+
+  final SettingsBloc settingsBloc;
+  HomeBloc(BuildContext context)
+      : this.settingsBloc = BlocProvider.of<SettingsBloc>(context),
+        super(HomeState.initialState);
 
   @override
   Stream<HomeState> mapEventToState(HomeEvent event) async* {
@@ -51,6 +58,28 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
           yield state.clone(matB: map);
         break;
 
+      case RandomizeMatrixAEvent:
+        yield state.clone(randomA: false);
+        final matA = Map<int, int>();
+        for (var i = 0; i < state.rowsA; i++) {
+          for (var j = 0; j < state.columnsA; j++) {
+            matA[i * state.columnsA + j] = Random().nextInt(75);
+          }
+        }
+        yield state.clone(matA: matA, randomA: true);
+        break;
+
+      case RandomizeMatrixBEvent:
+        yield state.clone(randomB: false);
+        final matB = Map<int, int>();
+        for (var i = 0; i < state.rowsB; i++) {
+          for (var j = 0; j < state.columnsB; j++) {
+            matB[i * state.columnsB + j] = Random().nextInt(75);
+          }
+        }
+        yield state.clone(matB: matB, randomB: true);
+        break;
+
       case CalculateEvent:
         final empty = Map<int, int>();
         for (var i = 0; i < state.rowsA * state.columnsB; i++) {
@@ -59,17 +88,7 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
         yield state.clone(index: 1, matComp: empty, matFPGA: empty);
         add(UpdateExecEvent('\$ computer matrix calculation started.....'));
         print('${state.matA}\n${state.matB}');
-        final orderedList = <int>[];
-        orderedList.addAll([state.rowsA, 0]);
-        orderedList.addAll([state.columnsA, 0]);
-        orderedList.addAll([state.columnsB, 0]);
-        orderedList.addAll([0, 0, 0, 0, 0, 0, 15, 0, 70]);
-        orderedList.addAll(state.matA.values.toList());
-        for (var i = 0; i < state.columnsB; i++) {
-          for (var j = 0; j < state.rowsB; j++) {
-            orderedList.add(state.matB[i + (j * state.columnsB)]);
-          }
-        }
+        final orderedList = settingsBloc.state.isMulti ? genMulti() : genSingle();
         final out = orderedList.map((e) => e.toRadixString(16).padLeft(2, '0')).toList();
         print(out);
         yield state.clone(matComp: matMultipication(state));
@@ -82,7 +101,6 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
         final output = Map<int, int>();
         final directory = Platform.resolvedExecutable;
         final cd = directory.trim().split("Release\\fpga_matrix_multiplier.exe")[0];
-        print(cd);
         final File file = File('${cd}simulation\\modelsim\\output_files\\res.txt');
         add(UpdateExecEvent('\$ reading result file..... ${cd}simulation\\modelsim\\output_files\\res.txt'));
         timer = Timer.periodic(Duration(seconds: 2), (timer) {
@@ -166,7 +184,6 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
   Future<void> writeMem(List<String> output) async {
     final directory = Platform.resolvedExecutable;
     final cd = directory.trim().split("Release\\fpga_matrix_multiplier.exe")[0];
-    print(cd);
     final File file = File('${cd}simulation\\modelsim\\initial_files\\data_mem.txt');
     final File fileOut = File('${cd}simulation\\modelsim\\output_files\\res.txt');
     await fileOut.writeAsString('');
@@ -174,6 +191,36 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     for (var mem in output) {
       await file.writeAsString('$mem\n', mode: FileMode.append);
     }
+  }
+
+  List<int> genSingle() {
+    final orderedList = <int>[];
+    orderedList.addAll([state.rowsA, 0]);
+    orderedList.addAll([state.columnsA, 0]);
+    orderedList.addAll([state.columnsB, 0]);
+    orderedList.addAll([0, 0, 0, 0, 0, 0, 15, 0, 70]);
+    orderedList.addAll(state.matA.values.toList());
+    for (var i = 0; i < state.columnsB; i++) {
+      for (var j = 0; j < state.rowsB; j++) {
+        orderedList.add(state.matB[i + (j * state.columnsB)]);
+      }
+    }
+    return orderedList;
+  }
+
+  List<int> genMulti() {
+    final orderedList = <int>[];
+    orderedList.addAll([state.rowsA]);
+    orderedList.addAll([state.columnsA]);
+    orderedList.addAll([state.columnsB]);
+    orderedList.addAll([4, 0, 0, 0, 0, 10, 70]);
+    orderedList.addAll(state.matA.values.toList());
+    for (var i = 0; i < state.columnsB; i++) {
+      for (var j = 0; j < state.rowsB; j++) {
+        orderedList.add(state.matB[i + (j * state.columnsB)]);
+      }
+    }
+    return orderedList;
   }
 
   // Future<Map<int, int>> readMem(HomeState state) async {
@@ -201,6 +248,7 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     final cd = directory.trim().split("Release\\fpga_matrix_multiplier.exe")[0];
     final start = 140;
     final end = start + (state.rowsA * state.columnsB * 2) - 1;
+    print(settingsBloc.state.doName);
     var controller = ShellLinesController();
     var shell = Shell(stdout: controller.sink, verbose: false, workingDirectory: '${cd}simulation\\modelsim');
     controller.stream.listen((event) {
@@ -210,7 +258,7 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     try {
       await shell.run('''
         # VSIM exec function
-        vsim -do "do FPGA_Project_run_msim_rtl_verilog.do; do {wave_files/waves.do}; radix -decimal; restart -force; run -all; mem display -addressradix d -dataradix decimal -wordsperline 10 /processor_tb/dut/data_mem1; mem save -outfile {output_files/res.txt} -dataradix unsigned -noaddress -start $start -end $end /processor_tb/dut/data_mem1 -wordsperline 1"
+        vsim -do "do ${settingsBloc.state.doName}; do {wave_files/waves.do}; radix -decimal; restart -force; run -all; mem display -addressradix d -dataradix unsigned -wordsperline 10 /processor_tb/dut/data_mem1; mem save -outfile {output_files/res.txt} -dataradix unsigned -noaddress -start $start -end $end /processor_tb/dut/data_mem1 -wordsperline 1"
         exit()
 ''');
     } on ShellException catch (_) {
