5fc9b262de45a387d1f5d9ec343277df22d58e77
Dinith Herath <dinithherath18332@gmail.com>
2021-07-12 02:12:31 +0530

    Added core logic ui

--- lib/settings/settings_page.dart
+++ lib/settings/settings_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fpga_matrix_multiplier/constants.dart';
+import 'package:fpga_matrix_multiplier/settings/bloc/settings_bloc.dart';
+import 'package:fpga_matrix_multiplier/settings/bloc/settings_state.dart';
 import 'package:fpga_matrix_multiplier/settings/components/core_switcher.dart';
+import 'package:fpga_matrix_multiplier/settings/components/num_cores.dart';
 import 'package:fpga_matrix_multiplier/settings/components/path.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,37 +15,48 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-              iconSize: 52,
-              icon: Icon(
-                Icons.arrow_back_sharp,
-                color: Constants.kHomeCard,
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            Text(
-              'Settings',
-              style: GoogleFonts.montserrat(
-                textStyle: TextStyle(
-                  fontSize: 60,
-                  color: Constants.kHomeCard,
-                  fontWeight: FontWeight.w600,
+      body: BlocBuilder<SettingsBloc, SettingsState>(
+        builder: (context, state) {
+          return SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  iconSize: 52,
+                  icon: Icon(
+                    Icons.arrow_back_sharp,
+                    color: Constants.kHomeCard,
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
+                Text(
+                  'Settings',
+                  style: GoogleFonts.montserrat(
+                    textStyle: TextStyle(
+                      fontSize: 60,
+                      color: Constants.kHomeCard,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                PathField(),
+                SizedBox(height: 20),
+                CoreSwitcher(),
+                SizedBox(height: 20),
+                AnimatedSwitcher(
+                  duration: Duration(milliseconds: 500),
+                  switchInCurve: Curves.ease,
+                  switchOutCurve: Curves.ease,
+                  child: state.isMulti ? NumCores() : SizedBox(),
+                ),
+              ],
             ),
-            PathField(),
-            SizedBox(height: 20),
-            CoreSwitcher(),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
